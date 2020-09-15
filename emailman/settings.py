484fbb19e17e2c5f20a9c714d295b7a37310a13e
Parth Pandya <34797335+parthpandyappp@users.noreply.github.com>
2020-09-15 19:14:39 +0530

    Update settings.py

--- emailman/settings.py
+++ emailman/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,5 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True 
 EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
